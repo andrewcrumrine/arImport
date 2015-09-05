88c0444125c526b5694c1d65a71747b72696601f
andrewcrumrine <andrewcrumrine@gmail.com>
2015-09-05 13:14:52 -0400

    AR Build: Successful leave objects

--- arBuilder.py
+++ arBuilder.py
@@ -12,9 +12,11 @@
 import csvCreator as csv
 import stringMan as s
 
-PAYMENT = "PMT"
-INVOICE = "INV"
-CREDIT = "CRM"
+PAYMENT 	= "PMT"
+INVOICE 	= "INV"
+CREDIT 		= "CRM"
+INV_TERM 	= '*'
+ACCT_TERM	= 'TOTAL'
 
 class ARCreator(csv.CSVCreator):
 	"""
@@ -29,8 +31,9 @@ class ARCreator(csv.CSVCreator):
 		self.account = None
 		self.headers = ['Customer','Invoice','Invoice Date','Transaction Date'\
 		,'Amount','Open Balance']
-		self.indices = {'Customer':[0,8],'Invoice':[0,8], 'Code':[8,12],\
-		'IDate':[12,21],'Type':[8,12],'TDate',[21,31]}
+		self.indices = {'Customer':[0,8],'Invoice':[0,8], 'Code':[8,13],\
+		'IDate':[13,23],'Type':[8,12],'TDate':[23,33],'Amount':[33,50],\
+		}
 
 	def __del__(self):
 		"""
@@ -43,8 +46,41 @@ class ARCreator(csv.CSVCreator):
 		"""
 	Accepts incoming string and manages writing to csv
 		"""
-		if self.account is None:
-			pass
+		self._setText(textIn)
+		if self.account is not None:
+			if not self.account.building:
+				self.printEntry()
+		else:
+			self._buildAccount(textIn)
+	
+	def _buildAccount(self,textIn):
+		"""
+	Method that manages the account building process
+		"""
+		if self.account is None and self._isCustomer():
+			customer = self.iterText('Customer')
+			self.account = OpenAccount(customer)
+
+		elif self.account is not None:
+			if self._isCustomer() and not self.account.building:
+				self.account = OpenAccount(customer)
+
+
+	def _isCustomer(self):
+		"""
+	Checks to see if the incoming text contains a customer number
+		"""
+		textIn = self.iterText('Customer')
+		textIn = s.removeSpaces(textIn)
+		if len(textIn) < 8:
+			return True
+		return False
+
+	def printEntry(self):
+		"""
+	Diagnostic tool to print entry
+		"""
+		pass
 
 class OpenAccount(object):
 	"""
@@ -57,21 +93,33 @@ class OpenAccount(object):
 	Initializes the OpenAccount class.  Needs the customer Id to be
 	inputed to initialize.
 		"""
-		self.customer = customer
+		self.customer = s.removeSpaces(customer)
 		self.invoices = []
 		self.building = True
 
-	def _setInvoice(self,invoice):
+	def _addInvoice(self,invoice,date):
 		"""
-	Sets invoice variable
+	Creates an invoice and adds it to the list
 		"""
-		self.invoice = invoice
+		newInvoice = Invoice(invoice,date)
+		if not newInvoice.building:
+			self.building = False
+			return
+		self.invoices.append(newInvoice)
+
+
+	def _addTransToInvoice(self,tranType,date,amount):
+		"""
+	Calls transaction builder to invoice object
+		"""
+		self.invoices[-1]._addTransaction(tranType,date,amount)
+
 
 class Invoice(object):
 	"""
 	This class is an invoice.
 	"""
-	def __init__(self,invoice):
+	def __init__(self,invoice,date):
 		"""
 	This initializes the invoice.
 		"""		
@@ -79,14 +127,10 @@ class Invoice(object):
 		self.items = []
 		self.transactions = []
 		self.balance = 0
-		self.date = ''
-		self.lateCode = 0
-
-	def _setDate(self,date):
-		"""
-	Sets date variable
-		"""
 		self.date = s.removeSpaces(date)
+		self.lateCode = 0
+		self.termKey = ACCT_TERM
+		self.building = self._isTerminator(invoice)
 
 	def _setLateCode(self,code):
 		"""
@@ -102,14 +146,25 @@ class Invoice(object):
 		newTrans = InvoiceTrans(self.invoice,transType)
 		newTrans.setDate(date)
 		newTrans.setAmount(amount)
+		if not newTrans.building:
+			self.building = False
+			return
 		self.transactions.append(newTrans)
 		self._correctBalance(newTrans)
 
-	def _correctBalence(self,tranIn):
+	def _correctBalance(self,tranIn):
 		"""
 	Adds the transaction amount to the bottom line
 		"""
 		self.balance += tranIn.amount
+
+	def _isTerminator(self,textIn):
+		"""
+	Checks to see if the new invoice is the terminator
+		"""
+		if textIn.find(self.termKey) == -1:
+			return False
+		return True
 
 class InvoiceTrans(object):
 	"""
@@ -124,6 +179,8 @@ class InvoiceTrans(object):
 		self.invoice = reference
 		self.transType = s.removeSpaces(transType)
 		self.amount = 0
+		self.termKey = INV_TERM
+		self.building = True
 
 	def setAmount(self,amount):
 		"""
@@ -131,10 +188,22 @@ class InvoiceTrans(object):
 		"""
 		amount = s.removeSpaces(amount)
 		amount = s.removeMinus(amount)
+		if self._isTerminator(amount):
+			self.building = False
+			return
 		if self.transType == PAYMENT or self.transType == CREDIT:
 			self.amount = -float(amount)
 		else:
 			self.amount = float(amount)
+
+	def _isTerminator(self,amount):
+		"""
+	Looks for the terminating key that determines when the invoice does
+	not have any additional transactions.
+		"""
+		if amount.find(self.termKey) == -1:
+			return False
+		return True
 
 	def setDate(self,date):
 		"""
